2f548349d8413343a4b04ab3269aeed077deb340
ryjmcnamee <ryjmcnamee@gmail.com>
2022-09-26 19:57:28 -0700

    add getHoroscope() function to create horoscope object

---
+++ assets/javascript/script.js
@@ -0,0 +1,29 @@
+
+
+function getHoroscope(requestedDate, requestedSign) {
+    // Below are exammple results from this function 
+    //
+    // "date_range": "May 21 - Jun 21",
+    // "current_date": "September 27, 2022",
+    // "description": "Make your plans now etc etc",
+    // "compatibility": "Aquarius",
+    // "mood": "Mellow",
+    // "color": "Navy Blue",
+    // "lucky_number": "45",
+    // "lucky_time": "1am"
+    //
+    // var horoscopeObjectExample = getHoroscope("today", "gemini");
+    //
+    // requestedDate options = "today", "yesterday", "tomorrow"
+    //
+    const horoscopeURL = 'https://aztro.sameerkumar.website/?sign=' + requestedSign + "&day=" + requestedDate;
+
+    fetch(horoscopeURL, {
+        method: 'POST'
+    })
+        .then(response => response.json())
+        .then(json => {
+            console.log(json)
+        });
+}
+

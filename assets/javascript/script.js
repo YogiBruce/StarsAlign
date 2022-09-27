d859a076c2a740b0ebb9da6982931069bc7e7724
ryjmcnamee <ryjmcnamee@gmail.com>
2022-09-26 21:01:06 -0700

    add event listner and code to update cards

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -26,12 +26,29 @@ function getHoroscope(requestedDate, requestedSign) {
             console.log(json)
         });
 }
+// Construct selector object
 var selectedHoroscope = document.getElementById("horoscopeSelector");
 
+// Event listener for selector up top
 selectedHoroscope.addEventListener("change",function selectSign(event){
+    // Populate horoscopes based on selected option
     var previousDayHoroscope = getHoroscope("yesterday",event.target.value);
     var currentDayHoroscope = getHoroscope("today",event.target.value);
     var nextDayHoroscope = getHoroscope("tomorrow",event.target.value);
+    // Populate element objects from DOM for dynamic change
+    var previousDayElement = document.getElementById("yesterdayHoroscope");
+    var currentDayElement = document.getElementById("todayHoroscope");
+    var nextDayElement = document.getElementById("tomorrowHoroscope");
+    
+    // Function to change data dynamically on event call
+    function fillOutHoroscopeCard(cardElement,cardData){
+        cardElement.getElementByTagName("p").textContent = cardData["description"];
+    }
+
+    //Run above function for three options
+    fillOutHoroscopeCard(previousDayElement,previousDayHoroscope);
+    fillOutHoroscopeCard(currentDayElement,currentDayHoroscope);
+    fillOutHoroscopeCard(nextDayElement,nextDayHoroscope);
 })
 
 
